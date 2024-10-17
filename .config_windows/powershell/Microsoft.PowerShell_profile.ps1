5f87ee51711d086c8564f256e295f25689c97334
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-10-17 16:20:24 +0200

    Update

--- .config_windows/powershell/Microsoft.PowerShell_profile.ps1
+++ .config_windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,10 @@
 oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/1_shell.omp.json' | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/atomic.omp.json" | Invoke-Expression
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 Import-Module -Name Terminal-Icons
 Import-Module z
+fnm env --use-on-cd | Out-String | Invoke-Expression
+& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" --print) -join "`n"))
